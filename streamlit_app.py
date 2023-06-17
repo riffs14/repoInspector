aa294731806959e0f194ec3c08fd17a1bb34c195
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2023-06-17 12:48:41 +0530

    undecleared variable error resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,18 @@ import json
 # Page title
 st.set_page_config(page_title='🦜🔗 Github Repo Quality Inspector')
 st.title('🦜🔗 Github Repo Quality Inspector')
-
+result=''
 github_link=st.text_input('Github Profile Link', type = 'default')
-submitted = st.form_submit_button('Submit')
-if submitted:
-    result = os.popen("github_link ").read()
-    json_object = json.loads(result)
-    list_repo=[]
-    for count,i in enumerate(json_object):
-        if i['visibility']=='public':
-            list_repo.append(i['html_url'])
-    #print(list_repo)
+with st.form('summarize_form', clear_on_submit=True):
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        result = os.popen("github_link ").read()
+        json_object = json.loads(result)
+        list_repo=[]
+        for count,i in enumerate(json_object):
+            if i['visibility']=='public':
+                list_repo.append(i['html_url'])
+        #print(list_repo)
 
 
 if len(result):
