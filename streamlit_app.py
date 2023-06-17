7687a9746668d7526f632c09a7083c58fa01a909
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2023-06-17 12:43:30 +0530

    submit button added

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,15 @@ st.set_page_config(page_title='🦜🔗 Github Repo Quality Inspector')
 st.title('🦜🔗 Github Repo Quality Inspector')
 
 github_link=st.text_input('Github Profile Link', type = 'default')
-result = os.popen("curl https://api.github.com/users/riffs14/repos ").read()
-json_object = json.loads(result)
-list_repo=[]
-for count,i in enumerate(json_object):
-    if i['visibility']=='public':
-        list_repo.append(i['html_url'])
-#print(list_repo)
+submitted = st.form_submit_button('Submit')
+if submitted:
+    result = os.popen("github_link ").read()
+    json_object = json.loads(result)
+    list_repo=[]
+    for count,i in enumerate(json_object):
+        if i['visibility']=='public':
+            list_repo.append(i['html_url'])
+    #print(list_repo)
 
 
 if len(result):
