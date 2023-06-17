e9d3c260a487fcdecfbeef1523e43244bbcf1bc7
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2023-06-17 12:52:35 +0530

    exception handle in app file

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,19 @@ result=''
 github_link=st.text_input('Github Profile Link', type = 'default')
 with st.form('summarize_form', clear_on_submit=True):
     submitted = st.form_submit_button('Submit')
-    if submitted:
-        result = os.popen("github_link ").read()
+    if submitted and len(github_link):
+        result = os.popen("curl github_link ").read()
         json_object = json.loads(result)
         list_repo=[]
-        for count,i in enumerate(json_object):
-            if i['visibility']=='public':
-                list_repo.append(i['html_url'])
-        #print(list_repo)
+        try:
+            for count,i in enumerate(json_object):
+                if i['visibility']=='public':
+                    list_repo.append(i['html_url'])
+            #print(list_repo)
+        except:
+            list_repo['Please enter a correct github link']
+
 
 
 if len(result):
     st.info(list_repo)
-else:
-    "curl not working"
