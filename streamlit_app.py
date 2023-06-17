e7fa16f058a9527daa0a97386f94759f82c599fc
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2023-06-17 13:15:31 +0530

    lol silly mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ result=''
 github_link=st.text_input('Enter Github Profile Link or username', type = 'default')
 github_link=os.path.basename(github_link)
 git_api_url='https://api.github.com/users/'
-
+list_repo=[]
 with st.form('summarize_form', clear_on_submit=True):
     submitted = st.form_submit_button('Submit')
     if submitted and len(github_link)>0:
         
-        list_repo=[]
+        
         try:
             #print(github_link)
             github_link=os.path.join(git_api_url,github_link,'repos')
@@ -29,7 +29,7 @@ with st.form('summarize_form', clear_on_submit=True):
                     list_repo.append(i['html_url'])
             #print(list_repo)
         except:
-            list_repo.append['Please enter a correct github link']
+            list_repo.append('Please enter a correct github link')
 
 
 
