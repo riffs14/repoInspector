f52b02610b79e91e0308b74cd558711409e220bd
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2023-06-17 13:09:08 +0530

    first version is complete : take userinput and retrieve link of all repo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,22 @@ import json
 st.set_page_config(page_title='🦜🔗 Github Repo Quality Inspector')
 st.title('🦜🔗 Github Repo Quality Inspector')
 result=''
-github_link=st.text_input('Github Profile Link', type = 'default')
+github_link=st.text_input('Enter Github Profile Link or username', type = 'default')
+github_link=os.path.basename(github_link)
+git_api_url='https://api.github.com/users/'
 with st.form('summarize_form', clear_on_submit=True):
     submitted = st.form_submit_button('Submit')
     if submitted and len(github_link)>0:
-        result = os.popen("curl github_link ").read()
-        json_object = json.loads(result)
+        
         list_repo=[]
         try:
+            #print(github_link)
+            github_link=os.path.join(git_api_url,github_link,'repos')
+            #print("******************************")
+            result = os.popen("curl "+github_link).read()
+            
+            #print(result)
+            json_object = json.loads(result)
             for count,i in enumerate(json_object):
                 if i['visibility']=='public':
                     list_repo.append(i['html_url'])
